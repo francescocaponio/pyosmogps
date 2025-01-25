c56d42c6a1bcfcee518e81507d8cd37ac344906c
Francesco Caponio <caponio@nuclearinstruments.eu>
2025-01-25 16:31:35 +0100

    Added a print to show that it is filtering the data

--- src/pyosmogps/data_filters.py
+++ src/pyosmogps/data_filters.py
@@ -4,11 +4,11 @@ import numpy as np
 from scipy.signal import butter, filtfilt
 
 
-def discard_resample_gps_data(global_gps_info, input_frequency, output_frequency):
+def discard_resample_gps_data(gps_info, input_frequency, output_frequency):
     """
     Resample the GPS data by discarding samples.
 
-    :param global_gps_info: List of dicts containing GPS data.
+    :param gps_info: List of dicts containing GPS data.
     :param input_frequency: Original frequency of the GPS data (Hz).
     :param output_frequency: Desired frequency of the GPS data (Hz).
     :return: Resampled list of dicts.
@@ -22,15 +22,15 @@ def discard_resample_gps_data(global_gps_info, input_frequency, output_frequency
         raise ValueError("Invalid step size. Check input and output frequencies.")
 
     # Subsample the data
-    resampled_data = global_gps_info[::step]
+    resampled_data = gps_info[::step]
     return resampled_data
 
 
-def lpf_resample_gps_data(global_gps_info, input_frame_rate, output_frequency):
+def lpf_resample_gps_data(gps_info, input_frame_rate, output_frequency):
     """
     Resample the GPS data using a low pass filter method.
 
-    :param global_gps_info: List of dicts containing GPS data.
+    :param gps_info: List of dicts containing GPS data.
     :param input_frame_rate: Original frame rate of the GPS data (Hz).
     :param output_frequency: Desired frequency of the GPS data (Hz).
     :return: Resampled list of dicts.
@@ -47,7 +47,7 @@ def lpf_resample_gps_data(global_gps_info, input_frame_rate, output_frequency):
     # Apply the filter to each field
     resampled_data = []
 
-    original_timestamps = [entry["timeinfo"] for entry in global_gps_info]
+    original_timestamps = [entry["timeinfo"] for entry in gps_info]
     total_duration = (original_timestamps[-1] - original_timestamps[0]).total_seconds()
     num_samples = int(total_duration * output_frequency)
     new_timestamps = [
@@ -62,12 +62,12 @@ def lpf_resample_gps_data(global_gps_info, input_frame_rate, output_frequency):
     for new_time in new_timestamps:
         new_entry = {"timeinfo": new_time}
 
-        for key in global_gps_info[0].keys():
+        for key in gps_info[0].keys():
             if key == "timeinfo":
                 continue
 
             # Extract values for the current key
-            values = np.array([entry[key] for entry in global_gps_info])
+            values = np.array([entry[key] for entry in gps_info])
 
             # Apply the low-pass filter
             filtered_values = filtfilt(b, a, values)
@@ -86,11 +86,11 @@ def lpf_resample_gps_data(global_gps_info, input_frame_rate, output_frequency):
     return resampled_data
 
 
-def linear_resample_gps_data(global_gps_info, input_frame_rate, output_frequency):
+def linear_resample_gps_data(gps_info, input_frame_rate, output_frequency):
     """
     Resample the GPS data using a linear interpolation method.
 
-    :param global_gps_info: List of dicts containing GPS data.
+    :param gps_info: List of dicts containing GPS data.
     :param input_frame_rate: Original frame rate of the GPS data (Hz).
     :param output_frequency: Desired frequency of the GPS data (Hz).
     :return: Resampled list of dicts.
@@ -98,18 +98,18 @@ def linear_resample_gps_data(global_gps_info, input_frame_rate, output_frequency
 
     # Calculate total duration and the new sample interval
     total_duration = (
-        global_gps_info[-1]["timeinfo"] - global_gps_info[0]["timeinfo"]
+        gps_info[-1]["timeinfo"] - gps_info[0]["timeinfo"]
     ).total_seconds()
     num_samples = int(total_duration * output_frequency)
     new_timestamps = [
-        global_gps_info[0]["timeinfo"] + timedelta(seconds=i / output_frequency)
+        gps_info[0]["timeinfo"] + timedelta(seconds=i / output_frequency)
         for i in range(num_samples)
     ]
 
     resampled_data = []
 
     # Extract original timestamps and values
-    original_timestamps = [entry["timeinfo"] for entry in global_gps_info]
+    original_timestamps = [entry["timeinfo"] for entry in gps_info]
     original_timestamps_seconds = np.array(
         [(t - original_timestamps[0]).total_seconds() for t in original_timestamps]
     )
@@ -118,12 +118,12 @@ def linear_resample_gps_data(global_gps_info, input_frame_rate, output_frequency
     for new_time in new_timestamps:
         new_entry = {"timeinfo": new_time}
 
-        for key in global_gps_info[0].keys():
+        for key in gps_info[0].keys():
             if key == "timeinfo":
                 continue
 
             # Extract values for the current key
-            values = np.array([entry[key] for entry in global_gps_info])
+            values = np.array([entry[key] for entry in gps_info])
 
             # Interpolate linearly
             new_value = np.interp(
@@ -139,21 +139,22 @@ def linear_resample_gps_data(global_gps_info, input_frame_rate, output_frequency
     return resampled_data
 
 
-def resample_gps_data(
-    global_gps_info, input_frame_rate, output_frequency, resampling_method
-):
+def resample_gps_data(gps_info, input_frame_rate, output_frequency, resampling_method):
+    """Resample the GPS data using the specified method."""
+
+    print("Resampling GPS data...")
 
     if resampling_method == "linear":
         resampled_data = linear_resample_gps_data(
-            global_gps_info, input_frame_rate, output_frequency
+            gps_info, input_frame_rate, output_frequency
         )
     elif resampling_method == "lpf":
         resampled_data = lpf_resample_gps_data(
-            global_gps_info, input_frame_rate, output_frequency
+            gps_info, input_frame_rate, output_frequency
         )
     elif resampling_method == "discard":
         resampled_data = discard_resample_gps_data(
-            global_gps_info, input_frame_rate, output_frequency
+            gps_info, input_frame_rate, output_frequency
         )
 
     return resampled_data
